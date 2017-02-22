5ffc7b32e02e6f1c9955edf9a544200ec36e554e
Aaron Smulktis <aaronsmulktis@gmail.com>
2017-02-21 18:36:01 -0800

    bring all DFP header scripts in here

--- ad-switcher.php
+++ ad-switcher.php
@@ -12,7 +12,20 @@ Author URI: http://thickmaterial.com
 
 function hook_header_scripts() {
     ?>
+			<!-- DFP script -->
 			<script type='text/javascript'>
+				var googletag = googletag || {};
+				googletag.cmd = googletag.cmd || [];
+				(function() {
+					var gads = document.createElement('script');
+					gads.async = true;
+					gads.type = 'text/javascript';
+					var useSSL = 'https:' == document.location.protocol;
+					gads.src = (useSSL ? 'https:' : 'http:') +
+						'//www.googletagservices.com/tag/js/gpt.js';
+					var node = document.getElementsByTagName('script')[0];
+					node.parentNode.insertBefore(gads, node);
+				})();
 
 				var isMobile = false; //initiate as false
 				// device detection
